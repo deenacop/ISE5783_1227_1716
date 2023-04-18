a16ad2f066e994935d93a05d1cdf582ffa686929
Deena <dcopperm@g.jct.ac.il>
2023-04-18 13:27:22 +0300

    bonus

--- unittests/geometries/CylinderTest.java
+++ unittests/geometries/CylinderTest.java
@@ -13,27 +13,21 @@ class CylinderTest {
      */
     @Test
     void testGetNormal() {
-        // ============ Equivalence Partitions Tests ==============
-        // TC01: There is a simple single test here
-        Cylinder cy = new Cylinder(
-                new Ray(new Point(0, 0, 1), new Vector(0, 0, 1)),
-                2,
-                3);
-        // test for point on first disk
-        assertEquals(new Vector(0, 0, 1), cy.getNormal(new Point(0, 1, 1)));
 
-        // test for point on second disk
-        assertEquals(new Vector(0, 0, 1), cy.getNormal(new Point(0, 1, 4)));
+            // ============ Equivalence Partitions Tests ==============
+            // TC01: There is a simple single test here (when point is on the top of the cylinder)
+            Cylinder pl = new Cylinder(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 1, 1d);
+            assertEquals(new Vector(0, 0, 1d), pl.getNormal(new Point(0, 0.5, 1)), "Bad normal to cylinder");
+            // TC02: There is a simple single test here (when point is on the base of the cylinder)
+            assertEquals(new Vector(0, 0, -1d), pl.getNormal(new Point(0, 0.5, 0)), "Bad normal to cylinder");
+            // TC03: There is a simple single test here (when point is on the surface of the cylinder)
+            assertEquals(new Vector(0, 1d, 0), pl.getNormal(new Point(0, 0.5, 0.5)), "Bad normal to cylinder");
 
+            // =============== Boundary Values Tests ==================
+            // TC04: There is a single boundary test here (between surface and base of cylinder)
+            assertNotEquals(pl.getNormal(new Point(0, 1, 0.00001)), pl.getNormal(new Point(0, 0.99999999, 0)), "Error in boundary test with base !");
+            // TC05: There is a single boundary test here (between surface and top of cylinder)
+            assertNotEquals(pl.getNormal(new Point(0, 1, 0.99999)), pl.getNormal(new Point(0, 0.99999999, 1)), "Error in boundary test with top !");
 
-        //test for point on body
-        assertEquals(new Vector(0, 1, 0), cy.getNormal(new Point(0, 2, 2)));
-
-        // ============ Boundary Value Tests ==============
-        // test for center point on second disk
-        assertEquals(new Vector(0, 0, 1), cy.getNormal(new Point(0, 0, 1)));
-
-        // test for center point on second disk
-        assertEquals(new Vector(0, 0, 1), cy.getNormal(new Point(0, 0, 4)));
     }
 }
