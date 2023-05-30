ce62ed102da9f2de09373dabfff39d2c2d3b318f
Deena <dcopperm@g.jct.ac.il>
2023-05-30 13:49:12 +0300

    view

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -4,6 +4,9 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static primitives.Util.isZero;
 
 /**
@@ -18,15 +21,42 @@ public class Cylinder extends Tube {
     final double height;
 
     /**
-     * Constructs a cylinder with a given height, axis ray, and radius.
+     * Fields represent the bottom base of the cylinder
+     */
+    protected final Plane bottomCap;
+    /**
+     * Fields represent the top base of the cylinder
+     */
+    protected final Plane topCap;
+
+
+
+    /**
+     * Constructor for cylinder
      *
-     * @param height  The height of the cylinder
-     * @param axisRay The axis ray of the cylinder
-     * @param radius  The radius of the cylinder
+     * @param axisRay parameter for axisRay field
+     * @param radius  parameter for radius field
+     * @param height  parameter for height field
      */
     public Cylinder(Ray axisRay, double height, double radius) {
         super(axisRay, radius);
+
+        if (height <= 0) {
+            throw new IllegalArgumentException("The height should be greater then 0");
+        }
+        if (radius <= 0) {
+            throw new IllegalArgumentException("The radius should be greater then 0");
+        }
+
         this.height = height;
+        Point p0 = axisRay.getP0();
+        Point p1 = axisRay.getPoint(height);
+        bottomCap = new Plane(p0, axisRay.getDir().scale(-1) /* Sets the normal directed outside of the cylinder */);
+        topCap = new Plane(p1, axisRay.getDir());
+    }
+
+    public double getHeight() {
+        return height;
     }
 
     /**
@@ -66,4 +96,98 @@ public class Cylinder extends Tube {
         Point o = p0.add(v.scale(t));  // o = p0 + t * v
         return point.subtract(o).normalize();
     }
+    @Override
+    public List<GeoPoint> findGeoIntersectionsHelper(Ray ray, double maxDistance) {
+        Point p0 = axisRay.getP0();
+        Point p1 = axisRay.getPoint(height);
+        List<GeoPoint> result = null;
+
+        // Find the infinite tube's intersections
+        List<GeoPoint> tubePoints = super.findGeoIntersectionsHelper(ray, maxDistance);
+
+        if (tubePoints != null) {
+            if (tubePoints.size() == 2) {
+                // Checks if the intersection points are on the cylinder
+                GeoPoint q0 = tubePoints.get(0);
+                GeoPoint q1 = tubePoints.get(1);
+                boolean q0Intersects = isBetweenCaps(q0.point);
+                boolean q1Intersects = isBetweenCaps(q1.point);
+
+                if (q0Intersects && q1Intersects) {
+                    return List.of(new GeoPoint(this, q0.point), new GeoPoint(this, q1.point));
+                }
+
+                if (q0Intersects) {
+                    result = new LinkedList<>();
+                    result.add(new GeoPoint(this, q0.point));
+                } else if (q1Intersects) {
+                    result = new LinkedList<>();
+                    result.add(new GeoPoint(this, q1.point));
+                }
+            }
+
+            if (tubePoints.size() == 1) {
+                // Checks if the intersection point is on the cylinder
+                GeoPoint q = tubePoints.get(0);
+                if (isBetweenCaps(q.point)) {
+                    result = new LinkedList<>();
+                    result.add(new GeoPoint(this, q.point));
+                }
+            }
+        }
+
+        // Finds the bottom cap's intersections
+        List<GeoPoint> cap0Point = bottomCap.findGeoIntersections(ray);
+        if (cap0Point != null) {
+            // Checks if the intersection point is on the cap
+            GeoPoint gp = cap0Point.get(0);
+            if (gp.point.distanceSquared(p0) < radius * radius) {
+                if (result == null) {
+                    result = new LinkedList<>();
+                }
+
+                result.add(new GeoPoint(this, gp.point));
+                if (result.size() == 2) {
+                    return result;
+                }
+            }
+        }
+
+        // Finds the top cap's intersections
+        List<GeoPoint> cap1Point = topCap.findGeoIntersections(ray);
+        if (cap1Point != null) {
+            // Checks if the intersection point is on the cap
+            GeoPoint gp = cap1Point.get(0);
+            if (gp.point.distanceSquared(p1) < radius * radius) {
+                if (result == null) {
+                    return List.of(new GeoPoint(this, gp.point));
+                }
+
+                result.add(new GeoPoint(this, gp.point));
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Helper function that checks if a points is between the two caps (not on them, even on the edge)
+     *
+     * @param p The point that will be checked.
+     * @return True if it is between the caps. Otherwise, false.
+     */
+    private boolean isBetweenCaps(Point p) {
+        Vector v0 = axisRay.getDir();
+        Point p0 = axisRay.getP0();
+        Point p1 = axisRay.getPoint(height);
+
+        // Checks against zero vector...
+        if (p.equals(p0) || p.equals(p1)) {
+            return false;
+        }
+
+        return v0.dotProduct(p.subtract(p0)) > 0 &&
+                v0.dotProduct(p.subtract(p1)) < 0;
+    }
+
 }
--- unittests/project/picturesTest.java
+++ unittests/project/picturesTest.java
@@ -1,17 +1,14 @@
 package project;
 
+import lighting.*;
 import scene.Scene;
 
 
 import static java.awt.Color.*;
 
 import geometries.*;
-import lighting.LightSource;
-import lighting.PointLight;
 import org.junit.jupiter.api.Test;
 
-import lighting.AmbientLight;
-import lighting.SpotLight;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
@@ -28,6 +25,7 @@ import java.util.List;
  */
 public class picturesTest {
     private Scene scene = new Scene("Test scene");
+
     @Test
     public void beautifulPictureScene() {
         Camera camera = new Camera(
@@ -48,40 +46,70 @@ public class picturesTest {
         scene.geometries.add(ground);
 
         // Sun
-        Geometry sun = new Sphere(100, new Point(500, 500, -800))
+        Geometry sun = new Sphere(150, new Point(400, 500, -800))
                 .setEmission(new Color(255, 255, 0)) // Yellow sun
                 .setMaterial(new Material().setKd(0).setKs(0).setShininess(100));
         scene.geometries.add(sun);
 
 
+        // Sun rays
+        int numRays = 16;
+        double rayLength = 400;
+
+        for (int i = 0; i < numRays; i++) {
+            double angle = (2 * Math.PI * i) / numRays;
+
+            Geometry ray = new Polygon(
+                    new Point(400, 500, -800),
+                    new Point(400 + rayLength * Math.cos(angle), 500 + rayLength * Math.sin(angle), -800),
+                    new Point(400 + rayLength * Math.cos(angle + Math.PI / 16), 500 + rayLength * Math.sin(angle + Math.PI / 16), -800))
+                    .setEmission(new Color(255, 255, 0)) // Yellow sun ray
+                    .setMaterial(new Material().setKd(0).setKs(0).setShininess(100));
+            scene.geometries.add(ray);
+        }
+
+// Chimney for the house
+        scene.geometries.add(new Polygon(
+                new Point(100, 100, -600),
+                new Point(100, 200, -600),
+                new Point(100, 200, -500),
+                new Point(100, 100, -500))
+                .setEmission(new Color(128, 0, 0)) // Maroon chimney
+                .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100)));
+
+
         // Spotlight from the sun
 
-        scene.lights.add(new PointLight(new Color(white), new Point(400, 400, -800)).setKl(0.00001).setKq(0.000005));
-
+        scene.lights.add(new PointLight(new Color(white), new Point(400, 350, -700)).setKl(0.00001).setKq(0.000003));
+        scene.lights.add(new DirectionalLight(new Color(0, 0, 0), new Vector(1, 1, -0.5)));
+        scene.lights.add(new SpotLight(new Color(0, 0, 0), new Vector(1, 1, -0.5), new Vector(1, 1, -0.5))
+                .setKl(0.001).setKq(0.0001));
+        scene.lights.add(new DirectionalLight(new Color(GREEN),new Vector(0,1,5)));
         // Clouds
         int numClouds = 20;
         int cloudRadius = 80;
         int cloudSpread = 200;
 
-        for (int i = 0; i < numClouds; i++) {
+        for (int j = 0; j < numClouds; j++) {
             double x = -500 + (Math.random() * cloudSpread);
             double y = 250 + (Math.random() * cloudSpread);
             double z = -600 + (Math.random() * cloudSpread);
 
             Geometry cloud = new Sphere(cloudRadius, new Point(x, y, z))
                     .setEmission(new Color(255, 255, 255)) // White clouds
-                    .setMaterial(new Material().setKd(0).setKs(0).setShininess(100));
+                    .setMaterial(new Material().setKd(0).setKs(0).setShininess(300));
             scene.geometries.add(cloud);
         }
 
 
         // House
-        scene.geometries.add(new Polygon(new Point(-200, -200, -400),
+        scene.geometries.add(new Polygon(
+                new Point(-200, -200, -400),
                 new Point(200, -200, -400),
                 new Point(200, 200, -400),
                 new Point(-200, 200, -400))
-                .setEmission(new Color(128, 0, 0)) // Maroon house
-                .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100)));
+                .setEmission(new Color(181, 101, 29)) // light brown house
+                .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(300)));
 
         // Add door to the house
         Geometry door = new Polygon(
@@ -89,9 +117,17 @@ public class picturesTest {
                 new Point(50, -200, -399),
                 new Point(50, 0, -399),
                 new Point(-50, 0, -399))
-                .setEmission(new Color(139, 69, 19)) // Brown door
+                .setEmission(new Color(100, 100, 100)) // Brown door
                 .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
         scene.geometries.add(door);
+
+        // Add door handle for the door
+        Geometry doorHandle = new Sphere(7, new Point(-20, -100, -398))
+                .setEmission(new Color(0, 0, 0)) // Black door handle
+                .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
+        scene.geometries.add(doorHandle);
+
+
 
         // Add window to the house
         Geometry window = new Polygon(
@@ -107,8 +143,71 @@ public class picturesTest {
         scene.geometries.add(new Triangle(new Point(-300, 200, -400),
                 new Point(0, 400, -400),
                 new Point(300, 200, -400))
-                .setEmission(new Color(139, 69, 19)) // Brown roof
+                .setEmission(new Color(128, 0, 0)) // Maroon roof
                 .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100)));
+// Fence
+        int fenceHeight = 150;
+        int fenceWidth = 20;
+        int fenceSpacing = 50;
+        int numFencePosts = 20;
+
+// Create horizontal fence rails
+        for (int i = 0; i < numFencePosts; i++) {
+            double x = -200 + (fenceSpacing * i);
+
+            Geometry rail1 = new Polygon(
+                    new Point(x, -200, -400),
+                    new Point(x + fenceWidth, -200, -400),
+                    new Point(x + fenceWidth, -200 + fenceHeight, -400),
+                    new Point(x, -200 + fenceHeight, -400))
+                    .setEmission(new Color(139, 69, 19)) // Brown fence rail
+                    .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
+            scene.geometries.add(rail1);
+
+            Geometry rail2 = new Polygon(
+                    new Point(-x, -200, -400),
+                    new Point(-(x + fenceWidth), -200, -400),
+                    new Point(-(x + fenceWidth), -200 + fenceHeight, -400),
+                    new Point(-x, -200 + fenceHeight, -400))
+                    .setEmission(new Color(139, 69, 19)) // Brown fence rail
+                    .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
+            scene.geometries.add(rail2);
+        }
+
+
+// Path
+        int pathWidth = 200;
+        int pathLength = 800;
+        int numStones = 3000;
+        int stoneRadius = 5;
+
+// Create path geometry
+        Geometry path = new Polygon(
+                new Point(-pathWidth / 2, -199, -pathLength / 2),
+                new Point(-pathWidth / 2, -199, pathLength / 2),
+                new Point(pathWidth / 2, -199, pathLength / 2),
+                new Point(pathWidth / 2, -199, -pathLength / 2))
+                .setEmission(new Color(139, 69, 19)) // Ground brown path
+                .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
+        scene.geometries.add(path);
+
+// Create stone spheres along the sides of the path
+        for (int i = 0; i < numStones; i++) {
+            double t = (double) i / (numStones - 1); // Parameter to interpolate between path points
+
+            double x = -pathWidth / 2 + (t * pathWidth);
+            double z = -pathLength / 2 + (Math.random() * pathLength);
+
+            // Add stones only on the sides of the path
+            if (x <= -pathWidth / 2 + stoneRadius || x >= pathWidth / 2 - stoneRadius) {
+                Geometry stone = new Sphere(stoneRadius, new Point(x, -199 + stoneRadius, z))
+                        .setEmission(new Color(105, 105, 105)) // Dark gray stone
+                        .setMaterial(new Material().setKd(0.6).setKs(0.4).setShininess(100));
+                scene.geometries.add(stone);
+            }
+        }
+
+
 
 
         ImageWriter imageWriter = new ImageWriter("beautiful_picture_scene", 1000, 1000);
@@ -117,6 +216,7 @@ public class picturesTest {
                 .renderImage();
         camera.writeToImage();
     }
+
 
     @Test
     public void initial_image_7() {
@@ -173,6 +273,7 @@ public class picturesTest {
                         .setMaterial(new Material().setKd(0.8).setKs(0.8).setShininess(30).setKr(0.8))
 
 
+
         );
 
         scene.lights.add(new PointLight(new Color(white), new Point(0, -650, 1053)).setKl(0.00001).setKq(0.000002));
@@ -186,3 +287,4 @@ public class picturesTest {
     }
 
 }
+
